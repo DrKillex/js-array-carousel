0f7395094a81c8bc905e37a734d110d91e552e9f
DrKillex <lcollaro1994@gmail.com>
2023-02-27 17:53:43 +0100

    finito no commenti no readme

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
 'use strict';
 
-/* <div class="item d-none show"><img src="img/01.webp" alt=""></div> */
-
 const imgList = ['01.webp', '02.webp', '03.webp', '04.webp', '05.webp'];
 
 const carousel = document.querySelector('.carousel');
-
+const thumbnail = document.querySelector('.thumbnail');
 
 let itemImg = '';
 let actualImg = 0;
@@ -13,23 +11,31 @@ let actualImg = 0;
 for (let i = 0; i < imgList.length; i++){
     itemImg += `<div class="item"><img src="img/${imgList[i]}" alt=""></div>`;   
 };
-carousel.innerHTML += itemImg;
 
-document.querySelector('.item').classList.add('show');
+carousel.innerHTML += itemImg;
+thumbnail.innerHTML += itemImg;
+
+document.querySelector('.carousel .item').classList.add('show');
+document.querySelector('.thumbnail .item').classList.add('clear');
 const arrowUp = document.querySelector('.arrow-up');
 const arrowDown = document.querySelector('.arrow-down');
-const divImg = document.querySelectorAll('.item');
+const divImg = document.querySelectorAll('.carousel .item');
+const thumbImg = document.querySelectorAll('.thumbnail .item');
 
 arrowDown.addEventListener ('click',
 function() {
     if(actualImg < imgList.length -1){
         divImg[actualImg].classList.remove('show');
-        actualImg++
+        thumbImg[actualImg].classList.remove('clear');
+        actualImg++;
         divImg[actualImg].classList.add('show');
+        thumbImg[actualImg].classList.add('clear');
     } else {
         divImg[actualImg].classList.remove('show');
-        actualImg = 0
+        thumbImg[actualImg].classList.remove('clear');
+        actualImg = 0;
         divImg[actualImg].classList.add('show');
+        thumbImg[actualImg].classList.add('clear');
     }
 });
 
@@ -37,11 +43,15 @@ arrowUp.addEventListener ('click',
 function() {
     if(actualImg > 0){
         divImg[actualImg].classList.remove('show');
-        actualImg = actualImg-1
+        thumbImg[actualImg].classList.remove('clear');
+        actualImg--;
         divImg[actualImg].classList.add('show');
+        thumbImg[actualImg].classList.add('clear');
     } else {
         divImg[actualImg].classList.remove('show');
-        actualImg = imgList.length-1
+        thumbImg[actualImg].classList.remove('clear');
+        actualImg = imgList.length-1;
         divImg[actualImg].classList.add('show');
+        thumbImg[actualImg].classList.add('clear');
     }
 });
